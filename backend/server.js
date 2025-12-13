dd608249cfd17be0d3d90b3eceeef870a9ed2e6a
Robert Pradilla <robertpradilla03@gmail.com>
2025-12-13 04:22:27 -0800

    Set up server

---
+++ backend/server.js
@@ -0,0 +1,15 @@
+import app from './app.js'
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const PORT = process.env.PORT 
+    
+app.listen(PORT, ()=>{
+    console.log(`Server running on port ${PORT}`);
+})
+app.get('/', (req, res) => {
+    res.json({message: "Hello"});
+  });
+
+  

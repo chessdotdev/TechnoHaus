218e237e6199207fca9e2611687cc1f35544ea15
Robert Pradilla <robertpradilla03@gmail.com>
2025-12-14 04:30:56 -0800

    Database connection

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,33 @@
 import app from './app.js'
 import dotenv from 'dotenv';
+import connectDB from "./config/database.js";
 
 dotenv.config();
 
-const PORT = process.env.PORT 
-    
-app.listen(PORT, ()=>{
-    console.log(`Server running on port ${PORT}`);
-})
-app.get('/', (req, res) => {
-    res.json({message: "Hello"});
-  });
+const PORT = process.env.PORT;
 
-  
+
+const startServer = async () => {
+
+    try {
+
+        await connectDB();
+            console.log("MONGO URI:", process.env.MONGODB_URI)
+
+        app.on("error", (error) => {
+            throw error;
+        });
+
+        app.listen(PORT, () => {
+            console.log(`Server running on port ${PORT}`);
+        })
+    } catch (error) {
+        console.error("MongoDB connection failed", err);
+
+    }
+
+}
+
+
+
+startServer();
